208c85c4c51a5962ac2e63eff037a5183c5ed9ed
Josh Lloyd <j.nevercast@gmail.com>
2014-03-21 18:13:26 +1300

    Begin Framing

---
+++ Networking/Frames/SegmentFrame.cs
@@ -0,0 +1,74 @@
+﻿using KMP.Networking.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KMP.Networking.Frames
+{
+    /// <summary>
+    /// Top Frame that encapsulates a packet and supports segmenting
+    /// </summary>
+    public class SegmentFrame
+    {
+        private static short NextSegmentNumber = 0;
+        private static short GenSegmentNumber()
+        {
+            if (NextSegmentNumber == short.MaxValue)
+            {
+                NextSegmentNumber = 0;
+                return short.MaxValue;
+            }
+            return NextSegmentNumber++;
+        }
+
+        public short SegmentNumber { get; private set; }
+        public SegmentPacketType Type { get; private set; }
+        public long UniqueNumber { get; private set; }
+
+        private SegmentFrame(short segNo, SegmentPacketType type, long unum)
+        {
+            this.SegmentNumber = segNo;
+            this.Type = type;
+            this.UniqueNumber = unum;
+        }
+
+        public static SegmentFrame CreateInit(long dataLength)
+        {
+            return new SegmentFrame(GenSegmentNumber(), SegmentPacketType.Init, dataLength);
+        }
+
+        public static SegmentFrame CreateSegment(SegmentFrame init, byte[] segmentData, long offset)
+        {
+            if (init.Type != SegmentPacketType.Init) throw new ArgumentException("Supplied frame is not an Init frame");
+            if (offset + segmentData.Length > init.UniqueNumber) throw new ArgumentException("Segment exceeds length of Init frame");
+            return new SegmentFrame(init.SegmentNumber, SegmentPacketType.Segment, offset);
+        }
+
+        public static SegmentFrame CreateChecksum(SegmentFrame init, byte[] data)
+        {
+            if (init.Type != SegmentPacketType.Init) throw new ArgumentException("Supplied frame is not an Init frame");
+            return new SegmentFrame(init.SegmentNumber, SegmentPacketType.Checksum, Hash.MD5(data));
+        }
+
+        public Boolean IsIntegrityOkay(byte[] receivedData)
+        {
+            if (Type == SegmentPacketType.Checksum)
+            {
+                return Hash.MD5(receivedData) == UniqueNumber;
+            }
+            return true;
+        }
+
+        public static SegmentFrame[] GetFrames(AbstractPacket packet)
+        {
+
+        }
+    }
+
+    public enum SegmentPacketType {
+        Init,
+        Segment,
+        Checksum
+    }
+}
---
+++ Networking/Util/Hash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KMP.Networking.Util
+{
+    public class Hash
+    {
+        public static long MD5(byte[] data)
+        {
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                var hash = md5.ComputeHash(data);
+                byte[] longBytes = new byte[8];
+                for (int i = 0, j = 0; i < hash.Length; i++, j++)
+                {
+                    if (j >= 8) j = 0;
+                    longBytes[j] ^= hash[i];
+                }
+                return BitConverter.ToInt64(longBytes, 0);
+            }
+        }
+    }
+}
